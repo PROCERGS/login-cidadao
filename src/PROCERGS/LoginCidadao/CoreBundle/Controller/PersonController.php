8607783a82bc434b76b524f07966bc9d8940a4dd
Guilherme Donato <guilhermednt@users.noreply.github.com>
2013-12-20 17:51:00 -0200

    Created /api/v1/person

---
+++ src/PROCERGS/LoginCidadao/CoreBundle/Controller/PersonController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PROCERGS\LoginCidadao\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+
+class PersonController extends Controller
+{
+
+    /**
+     * @Route("/person")
+     * @Template()
+     */
+    public function selfAction()
+    {
+        $user = $this->get('security.context')->getToken()->getUser();
+
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new GetSetMethodNormalizer());
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $json = $serializer->serialize($user, 'json');
+        die($json);
+    }
+
+}
