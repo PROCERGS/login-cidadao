63fcd91ac1b1956d0b1df521e30f882648404581
unknown <ricardo-jesus@PRO-PAE-0299.PROCERGS.REDERS>
2014-02-06 19:13:44 -0200

    init register layout

--- src/PROCERGS/LoginCidadao/CoreBundle/Form/Type/RegistrationFormType.php
+++ src/PROCERGS/LoginCidadao/CoreBundle/Form/Type/RegistrationFormType.php
@@ -11,12 +11,13 @@ class RegistrationFormType extends BaseType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);
-        
-        $builder->add('name');
+
+        $builder->add('firstName');
+        $builder->add('surname');
         $builder->add('birthdate', 'birthday');
         $builder->add('cep');
     }
-    
+
     public function getName()
     {
         return 'procergs_person_registration';
