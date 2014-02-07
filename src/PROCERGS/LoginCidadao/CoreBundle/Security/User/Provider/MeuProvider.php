2e293f9c4dc6bcedf5de87ac41624a811599a3b9
lordvacilaoerrocerto <lordvacilaoerrocerto@hotmail.com>
2014-02-07 08:39:12 -0200

    changed userprovider

---
+++ src/PROCERGS/LoginCidadao/CoreBundle/Security/User/Provider/MeuProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace PROCERGS\LoginCidadao\CoreBundle\Security\User\Provider;
+
+use FOS\UserBundle\Security\UserProvider;
+
+class MeuProvider extends UserProvider
+{
+    
+    /**
+     * Finds a user by username or em-amil or cpf.
+     *
+     * This method is meant to be an extension point for child classes.
+     *
+     * @param string $username
+     *
+     * @return UserInterface|null
+     */
+    protected function findUser($username)
+    {
+        return $this->userManager->findUserByUsernameOrEmail($username);
+    }
+}
