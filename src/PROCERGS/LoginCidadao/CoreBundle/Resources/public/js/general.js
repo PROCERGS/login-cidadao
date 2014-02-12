280126874d0eae2afab044b8db31c6f4e9fe2cae
ricardopdj <pareja.r@gmail.com>
2014-02-12 17:50:44 -0200

    front masks and ie fix

--- src/PROCERGS/LoginCidadao/CoreBundle/Resources/public/js/general.js
+++ src/PROCERGS/LoginCidadao/CoreBundle/Resources/public/js/general.js
@@ -1,5 +1,13 @@
 $(function() {
 
+    // add bootstrap classes to forms
     $('.form-group input, .form-group select').not('.form-control').addClass('form-control');
+    $('.form-group .input-error').not(':empty').parent('.form-group').addClass('has-error');
+
+    // maks inputs
+    $('#fos_user_registration_form_cep').mask('00000-000');
+
+    $('#fos_user_registration_form_cpf').mask('000.000.000-00');
+
 
 });
