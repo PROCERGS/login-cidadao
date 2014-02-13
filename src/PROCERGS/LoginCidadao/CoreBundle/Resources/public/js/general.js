81664954bd58aa954e770537c299a6b137430ce4
ricardopdj <pareja.r@gmail.com>
2014-02-13 18:44:03 -0200

    front application list

--- src/PROCERGS/LoginCidadao/CoreBundle/Resources/public/js/general.js
+++ src/PROCERGS/LoginCidadao/CoreBundle/Resources/public/js/general.js
@@ -10,4 +10,18 @@ $(function() {
     $('#fos_user_registration_form_cpf').mask('000.000.000-00');
 
 
+    // switch display application list
+    $('.btn-app-display').on('click', function() {
+        if ( !$(this).hasClass('active') ) {
+            $('.btn-app-display').removeClass('active');
+            $(this).addClass('active');
+
+            $('#applications').css({'margin-top': '-30px', 'opacity' : 0 });
+            setTimeout(function () {
+                $('#applications').css({'margin-top' : 0, 'opacity' : 1}).toggleClass('list');
+            }, 800);
+        }
+    });
+
+
 });
