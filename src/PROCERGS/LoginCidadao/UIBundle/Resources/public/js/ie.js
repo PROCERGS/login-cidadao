1117eec5e1569a8cd46f43d24645bf5cfd977b30
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-02-13 17:11:06 -0200

    It's time for UIBundle to go...

--- src/PROCERGS/LoginCidadao/UIBundle/Resources/public/js/ie.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    // detect IE to apply css
-    function getIe(){var e=-1;if(navigator.appName=="Microsoft Internet Explorer"){var t=navigator.userAgent;var n=new RegExp("MSIE ([0-9]{1,}[.0-9]{0,})");if(n.exec(t)!==null){e=parseFloat(RegExp.$1)}$("body").addClass("ie-"+e)}}getIe();
-
-    //plaholder support
-    $('input, textarea').placeholder();
-});
