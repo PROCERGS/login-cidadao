b40f3e4ef6de28fe409f108e29257d0293a40c3f
victorvis <victorviss@gmail.com>
2014-02-14 14:33:51 -0200

    validate cpf and cep if not empty

--- src/PROCERGS/Generic/ValidationBundle/Validator/Constraints/CEPValidator.php
+++ src/PROCERGS/Generic/ValidationBundle/Validator/Constraints/CEPValidator.php
@@ -8,30 +8,29 @@ use Symfony\Component\Validator\ConstraintValidator;
 /**
  * @Annotation
  */
-class CEPValidator extends ConstraintValidator
-{
+class CEPValidator extends ConstraintValidator {
 
     /**
      * Currently only checks if the value is numeric.
      * @param string $cep
      * @return boolean
      */
-    public static function isCEPValid($cep)
-    {
+    public static function isCEPValid($cep) {
         $cep = preg_replace('/[^0-9]/', '', $cep);
         if (!is_numeric($cep)) {
             return false;
         }
     }
 
-    public static function checkLength($cep)
-    {
+    public static function checkLength($cep) {
         $cep = preg_replace('/[^0-9]/', '', $cep);
         return strlen($cep) == 8;
     }
 
-    public function validate($value, Constraint $constraint)
-    {
+    public function validate($value, Constraint $constraint) {
+        if (!isset($value)) {
+            return;
+        }
         if (!self::checkLength($value)) {
             $this->context->addViolation($constraint->lengthMessage);
         }
