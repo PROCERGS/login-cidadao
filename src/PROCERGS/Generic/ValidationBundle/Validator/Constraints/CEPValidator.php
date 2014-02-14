2343f4dd85a7ce55eb695c0e8d5f3b67459e52dc
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-02-14 12:00:56 -0200

    Fixed check for CEP...

--- src/PROCERGS/Generic/ValidationBundle/Validator/Constraints/CEPValidator.php
+++ src/PROCERGS/Generic/ValidationBundle/Validator/Constraints/CEPValidator.php
@@ -19,9 +19,7 @@ class CEPValidator extends ConstraintValidator
     public static function isCEPValid($cep)
     {
         $cep = preg_replace('/[^0-9]/', '', $cep);
-        if (!is_numeric($cep)) {
-            return false;
-        }
+        return is_numeric($cep);
     }
 
     public static function checkLength($cep)
