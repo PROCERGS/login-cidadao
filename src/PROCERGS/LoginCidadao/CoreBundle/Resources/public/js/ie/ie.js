525619cb7e3c877535467144a70beefbf9e35594
ricardopdj <pareja.r@gmail.com>
2014-02-14 16:15:31 -0200

    fix application ie7-8

---
+++ src/PROCERGS/LoginCidadao/CoreBundle/Resources/public/js/ie/ie.js
@@ -0,0 +1,16 @@
+$(function() {
+    // detect IE to apply css
+    // function getIe(){var e=-1;if(navigator.appName=="Microsoft Internet Explorer"){var t=navigator.userAgent;var n=new RegExp("MSIE ([0-9]{1,}[.0-9]{0,})");if(n.exec(t)!==null){e=parseFloat(RegExp.$1)}$("body").addClass("ie-"+e);$('input, textarea').placeholder();}}getIe();
+
+    // ie 8-7 onoff support
+    $('.onoffswitch-inner').html('<i class="before">Ativado</i><i class="after">Desativado</i>');
+    $('.onoffswitch-label').click(function(){
+        $(this).parent().toggleClass('onoffswitch-checked');
+    });
+
+    // placeholder support
+    $('input, textarea').placeholder();
+
+
+
+});
