f50717cc16bfb6b62cb7e04d8d552ab0a1f80a61
ricardopdj <pareja.r@gmail.com>
2014-02-14 17:03:07 -0200

    add js fix responsive to register page

--- src/PROCERGS/LoginCidadao/CoreBundle/Resources/public/js/ie/ie.js
+++ src/PROCERGS/LoginCidadao/CoreBundle/Resources/public/js/ie/ie.js
@@ -2,6 +2,8 @@ $(function() {
     // detect IE to apply css
     // function getIe(){var e=-1;if(navigator.appName=="Microsoft Internet Explorer"){var t=navigator.userAgent;var n=new RegExp("MSIE ([0-9]{1,}[.0-9]{0,})");if(n.exec(t)!==null){e=parseFloat(RegExp.$1)}$("body").addClass("ie-"+e);$('input, textarea').placeholder();}}getIe();
 
+    console.log("JS support IE");
+
     // ie 8-7 onoff support
     $('.onoffswitch-inner').html('<i class="before">Ativado</i><i class="after">Desativado</i>');
     $('.onoffswitch-label').click(function(){
---
+++ src/PROCERGS/LoginCidadao/CoreBundle/Resources/public/js/register/register.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('#fos_user_registration_form_username').blur(function(){
+        $elm = $(this);
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: validateUsernameUrl,
+            data: { username: $(this).val() }
+        })
+        .done(function(result) {
+            if(result.valid){
+                $elm.parent().removeClass('has-error').find('.input-error').html("");
+            }else{
+                $elm.parent().addClass('has-error').find('.input-error').html('<ul><li>'+result.message+'</li></ul>');
+            }
+            return false;
+        });
+    });
+});
+
+$(window).resize(function() {
+    var pageWidth = $(window).width();
+
+    var register = $('#register-aside');
+    if( $(window).width()< 992){
+        register.remove();
+        $('#sign-up').after(register);
+    } else {
+        register.remove();
+        $('#sign-up').before(register);
+    }
+
+});
