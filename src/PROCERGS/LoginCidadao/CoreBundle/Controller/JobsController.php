5b85ddfa2b84983fc982e574a055b859fdea6793
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-02-20 11:00:18 -0300

    Added 'flood' protection. closes #1

--- src/PROCERGS/LoginCidadao/CoreBundle/Controller/JobsController.php
+++ src/PROCERGS/LoginCidadao/CoreBundle/Controller/JobsController.php
@@ -9,12 +9,13 @@ use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\Security\Core\Exception\DisabledException;
+use PROCERGS\LoginCidadao\CoreBundle\Entity\SentEmail;
 
 class JobsController extends Controller
 {
 
     /**
-     * @Route("/job/cpf-check")
+     * @Route("/job/cpf-reminder")
      * @Template()
      */
     public function cpfCheckAction()
@@ -24,19 +25,32 @@ class JobsController extends Controller
         $subject = $translator->trans('cpf_reminder.subject');
 
         $from = $this->container->getParameter('mailer_sender_mail');
-        $repo = $this->getDoctrine()
-            ->getManager()
+        $em = $this->getDoctrine()->getEntityManager();
+        $personRepo = $this->getDoctrine()
             ->getRepository('PROCERGSLoginCidadaoCoreBundle:Person');
-        $users = $repo->findAllPendingCPFUntilDate(new \DateTime());
+        $emailRepo = $this->getDoctrine()
+            ->getRepository('PROCERGSLoginCidadaoCoreBundle:SentEmail');
+
+        $users = $personRepo->findAllPendingCPFUntilDate(new \DateTime());
+        $todayMail = $emailRepo->findAllSentInTheLast24h(true);
 
         $mailCount = 0;
         foreach ($users as $user) {
-            $message = \Swift_Message::newInstance()->setSubject($subject)
-                ->setFrom($from)
-                ->setTo($user->getEmailCanonical())
-                ->setBody($this->renderView('PROCERGSLoginCidadaoCoreBundle:Jobs:cpf_check.txt.twig', compact('user')));
+            $to = $user->getEmailCanonical();
+            if (array_key_exists($to, $todayMail)) {
+                continue;
+            }
 
-            if ($this->get('mailer')->send($message)) {
+            $email = new SentEmail();
+            $email->setType('cpf-reminder')
+                ->setSubject($subject)
+                ->setSender($from)
+                ->setReceiver($to)
+                ->setMessage($this->renderView('PROCERGSLoginCidadaoCoreBundle:Jobs:cpf_check.txt.twig', compact('user')));
+
+            if ($this->get('mailer')->send($email->getSwiftMail())) {
+                $em->persist($email);
+                $em->flush();
                 $mailCount++;
             }
         }
