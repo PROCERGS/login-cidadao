591005089a80aa2ad073a3bed7cb4556c96ddbf3
ricardopdj <pareja.r@gmail.com>
2014-02-24 15:06:00 -0300

    change navbar html

--- src/PROCERGS/LoginCidadao/CoreBundle/Resources/public/js/navbar.js
+++ src/PROCERGS/LoginCidadao/CoreBundle/Resources/public/js/navbar.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-    $(".navbar .navbar-nav .btn.login").click(function(){
+    $(".navbar .btn-login").click(function(){
         var url = $(this).attr('href');
         window.open(url, '', "width=600,height=450");
         return false;
