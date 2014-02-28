8df9f1c9ab84f99bbff6a8ef7e740bb8c168aeb8
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-02-28 11:37:08 -0300

    Added mobile to form and moved some controls. #60

--- src/PROCERGS/LoginCidadao/CoreBundle/Form/Type/ProfileFormType.php
+++ src/PROCERGS/LoginCidadao/CoreBundle/Form/Type/ProfileFormType.php
@@ -12,29 +12,37 @@ class ProfileFormType extends BaseType
     {
         parent::buildForm($builder, $options);
 
-        $builder->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'));
-        $builder->add('email', 'email', array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'));
-        $builder->add('firstName', 'text', array('label' => 'form.firstName', 'translation_domain' => 'FOSUserBundle'));
-        $builder->add('surname', 'text', array('label' => 'form.surname', 'translation_domain' => 'FOSUserBundle'));
-        $builder->add('cep', null, array('required' => false, 'label' => 'form.cep', 'translation_domain' => 'FOSUserBundle'));
-        $builder->add('cpf', null, array('required' => false, 'label' => 'form.cpf', 'translation_domain' => 'FOSUserBundle') );
-        $builder->add('birthdate', 'birthday', array(
-            'required' => false,
-            'format' => 'dd MMMM yyyy',
-            'widget' => 'choice',
-            'years' => range(date('Y'), date('Y')-70),
-            'label' => 'form.birthdate', 
-            'translation_domain' => 'FOSUserBundle')
-        );
-        $builder->add('city', 'entity', array(
-            'class' => 'PROCERGSLoginCidadaoCoreBundle:City',
-            'property' => 'name',
-            'required' => false,
-            'label' => 'form.city',
-            'translation_domain' => 'FOSUserBundle'
-        ));
-        
-        
+        $builder->add('username', null,
+                        array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
+                ->add('email', 'email',
+                        array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+                ->add('firstName', 'text',
+                        array('label' => 'form.firstName', 'translation_domain' => 'FOSUserBundle'))
+                ->add('surname', 'text',
+                        array('label' => 'form.surname', 'translation_domain' => 'FOSUserBundle'))
+                ->add('cep', null,
+                        array('required' => false, 'label' => 'form.cep', 'translation_domain' => 'FOSUserBundle'))
+                ->add('cpf', null,
+                        array('required' => false, 'label' => 'form.cpf', 'translation_domain' => 'FOSUserBundle'))
+                ->add('birthdate', 'birthday',
+                        array(
+                    'required' => false,
+                    'format' => 'dd MMMM yyyy',
+                    'widget' => 'choice',
+                    'years' => range(date('Y'), date('Y') - 70),
+                    'label' => 'form.birthdate',
+                    'translation_domain' => 'FOSUserBundle')
+                )
+                ->add('city', 'entity',
+                        array(
+                    'class' => 'PROCERGSLoginCidadaoCoreBundle:City',
+                    'property' => 'name',
+                    'required' => false,
+                    'label' => 'form.city',
+                    'translation_domain' => 'FOSUserBundle'
+                ))
+                ->add('mobile', null,
+                        array('required' => false, 'label' => 'form.mobile', 'translation_domain' => 'FOSUserBundle'));
     }
 
     public function getName()
