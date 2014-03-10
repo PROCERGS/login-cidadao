29e58206afeacc165c2c82f1a5fa93a34874e976
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-03-10 10:36:58 -0300

    Closes #79

--- src/PROCERGS/LoginCidadao/CoreBundle/Form/Type/ChangePasswordFormType.php
+++ src/PROCERGS/LoginCidadao/CoreBundle/Form/Type/ChangePasswordFormType.php
@@ -4,33 +4,42 @@ namespace PROCERGS\LoginCidadao\CoreBundle\Form\Type;
 
 use Symfony\Component\Form\FormBuilderInterface;
 use FOS\UserBundle\Form\Type\ChangePasswordFormType as BaseType;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class ChangePasswordFormType extends BaseType
 {
 
+    private $context;
+
+    public function __construct($class, SecurityContextInterface $context)
+    {
+        parent::__construct($class);
+        $this->context = $context;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        parent::buildForm($builder, $options);
-        
-        $builder->add('plainPassword', 'repeated', array(
-            'type' => 'password',
-            'options' => array('attr' => array('class' => 'form-control'), 'translation_domain' => 'FOSUserBundle'),
-            'first_options' => array('label' => 'form.new_password'),
-            'second_options' => array('label' => 'form.new_password_confirmation'),
-            'invalid_message' => 'fos_user.password.mismatch',
-        ));
+        $user = $this->context->getToken()->getUser();
 
-        $builder->add('plainPassword', 'repeated', array(
-            'type' => 'password',
-            'options' => array('attr' => array('class' => 'form-control'), 'translation_domain' => 'FOSUserBundle'),
-            'first_options' => array('label' => 'form.new_password'),
-            'second_options' => array('label' => 'form.new_password_confirmation'),
-            'invalid_message' => 'fos_user.password.mismatch',
-        ));
+        $emptyPassword = strlen($user->getPassword()) == 0;
+        if (!$emptyPassword) {
+            parent::buildForm($builder, $options);
+        } else {
+            $builder->add('plainPassword', 'repeated',
+                    array(
+                'type' => 'password',
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'form.new_password'),
+                'second_options' => array('label' => 'form.new_password_confirmation'),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ));
+        }
     }
 
     public function getName()
     {
         return 'procergs_change_password';
     }
+
 }
