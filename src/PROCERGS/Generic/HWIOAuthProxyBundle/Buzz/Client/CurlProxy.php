4dc363837062622923f1dee088c9e9842c77908f
lordvacilaoerrocerto <lordvacilaoerrocerto@hotmail.com>
2014-03-11 17:27:00 -0300

    smal fix to work with php 5.3.3

--- src/PROCERGS/Generic/HWIOAuthProxyBundle/Buzz/Client/CurlProxy.php
+++ src/PROCERGS/Generic/HWIOAuthProxyBundle/Buzz/Client/CurlProxy.php
@@ -3,6 +3,11 @@
 namespace PROCERGS\Generic\HWIOAuthProxyBundle\Buzz\Client;
 
 use Buzz\Client\Curl;
+use Buzz\Message\MessageInterface;
+use Buzz\Message\RequestInterface;
+use Buzz\Exception\ClientException;
+use Buzz\Exception\LogicException;
+
 
 class CurlProxy extends Curl
 {
@@ -21,5 +26,27 @@ class CurlProxy extends Curl
             $this->setProxy($proxy);
         }
     }
+    
+    protected static function populateResponse($curl, $raw, MessageInterface $response)
+    {
+        $pos = explode("\r\n\r\n", $raw);
+        
+        $response->setContent(array_pop($pos));
+        $response->setHeaders(static::getLastHeaders(array_pop($pos)));
+    }
+
+    protected static function getLastHeaders($raw)
+    {
+        $headers = array();
+        foreach (preg_split('/(\\r?\\n)/', $raw) as $header) {
+            if ($header) {
+                $headers[] = $header;
+            } else {
+                $headers = array();
+            }
+        }
+    
+        return $headers;
+    }    
 
 }
