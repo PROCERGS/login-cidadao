1c976d9e41b3529f3f83f1592c8ebdab7812428b
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-03-13 15:06:38 -0300

    Removed useless code. (I guess... :P)

--- src/PROCERGS/Generic/TwitterOAuthProxiedBundle/PROCERGSGenericTwitterOAuthProxiedBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace PROCERGS\Generic\TwitterOAuthProxiedBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class PROCERGSGenericTwitterOAuthProxiedBundle extends Bundle
-{
-}
