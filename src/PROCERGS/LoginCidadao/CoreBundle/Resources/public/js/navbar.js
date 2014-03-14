5236c37a7adf4ef1e76e26a03f118670c03910a5
ricardopdj <pareja.r@gmail.com>
2014-03-14 09:49:05 -0300

    removing login popup

--- src/PROCERGS/LoginCidadao/CoreBundle/Resources/public/js/navbar.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function(){
-    $(".navbar .btn-login").click(function(){
-        var url = $(this).attr('data-href');
-        window.open(url, '', "width=600,height=450");
-        return false;
-    });
-});
