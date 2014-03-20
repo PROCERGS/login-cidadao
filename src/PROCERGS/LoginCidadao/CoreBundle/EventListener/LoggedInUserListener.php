a5ce04acb86a6a3a8d097d1ab4e6a8df76e8de4c
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-03-20 10:39:14 -0300

    Fixes #89. Thanks to http://stackoverflow.com/a/17582027/2698557

--- src/PROCERGS/LoginCidadao/CoreBundle/EventListener/LoggedInUserListener.php
+++ src/PROCERGS/LoginCidadao/CoreBundle/EventListener/LoggedInUserListener.php
@@ -7,18 +7,22 @@ use Symfony\Component\HttpKernel\HttpKernel;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class LoggedInUserListener
 {
 
     private $context;
     private $router;
+    private $session;
 
     public function __construct(SecurityContextInterface $context,
-                                RouterInterface $router)
+                                RouterInterface $router,
+                                SessionInterface $session)
     {
         $this->context = $context;
         $this->router = $router;
+        $this->session = $session;
     }
 
     public function onKernelRequest(GetResponseEvent $event)
@@ -33,7 +37,17 @@ class LoggedInUserListener
 
         $_route = $event->getRequest()->attributes->get('_route');
         if ($this->context->isGranted('IS_AUTHENTICATED_FULLY') && $_route == 'lc_home') {
-            $url = $this->router->generate('fos_user_profile_edit');
+            $key = '_security.main.target_path'; #where "main" is your firewall name
+            //check if the referer session key has been set
+            if ($this->session->has($key)) {
+                //set the url based on the link they were trying to access before being authenticated
+                $url = $this->session->get($key);
+
+                //remove the session key
+                $this->session->remove($key);
+            } else {
+                $url = $this->router->generate('fos_user_profile_edit');
+            }
             $event->setResponse(new RedirectResponse($url));
         }
     }
