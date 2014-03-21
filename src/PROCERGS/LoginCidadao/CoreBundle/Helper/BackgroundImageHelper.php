ef974a55b56e9f2225b9f0bb5192629106051402
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-03-21 17:41:51 -0300

    Added default image

--- src/PROCERGS/LoginCidadao/CoreBundle/Helper/BackgroundImageHelper.php
+++ src/PROCERGS/LoginCidadao/CoreBundle/Helper/BackgroundImageHelper.php
@@ -3,6 +3,7 @@
 namespace PROCERGS\LoginCidadao\CoreBundle\Helper;
 
 use Doctrine\ORM\EntityManager;
+use PROCERGS\LoginCidadao\CoreBundle\Entity\BackgroundImage;
 
 class BackgroundImageHelper
 {
@@ -12,11 +13,13 @@ class BackgroundImageHelper
      * @var EntityManager
      */
     private $em;
+    private $default;
     public $backgroundImage;
 
-    public function __construct(EntityManager $em)
+    public function __construct(EntityManager $em, $author, $url, $file)
     {
         $this->em = $em;
+        $this->default = array('author' => $author, 'url' => $url, 'file' => $file);
         $this->setRandomImage();
     }
 
@@ -28,8 +31,17 @@ class BackgroundImageHelper
     public function setRandomImage()
     {
         $all = $this->getRepository()->findAll();
-        shuffle($all);
-        $this->backgroundImage = reset($all);
+        if (!empty($all)) {
+            shuffle($all);
+            $this->backgroundImage = reset($all);
+        } else {
+            $image = new BackgroundImage();
+            $image->setAuthor($this->default['author']);
+            $image->setFile($this->default['file']);
+            $image->setUrl($this->default['url']);
+
+            $this->backgroundImage = $image;
+        }
     }
 
 }
