0e2f146144cd85fb02da4c82b1760e01fbfe2721
ricardopdj <pareja.r@gmail.com>
2014-03-27 16:13:10 -0300

    add read notifications

--- src/PROCERGS/LoginCidadao/CoreBundle/Controller/NotificationController.php
+++ src/PROCERGS/LoginCidadao/CoreBundle/Controller/NotificationController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use PROCERGS\LoginCidadao\CoreBundle\Entity\Notification;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class NotificationController extends Controller
 {
@@ -28,9 +30,31 @@ class NotificationController extends Controller
      * @Route("/read", name="lc_notifications_read")
      * @Template()
      */
-    public function readAction()
+    public function readAction(Request $request)
     {
+        $notificationId = $request->get('notification');
 
+        $em = $this->getDoctrine()->getManager();
+        $notifications = $em->getRepository('PROCERGSLoginCidadaoCoreBundle:Notification');
+
+        $data = array('status' => 0, 'message' => 'Error');
+
+        try {
+            $notification = $notifications->find($notificationId);
+            if ($notification && !$notification->isExtreme()) {
+                $notification->setIsRead(true);
+                $em->flush();
+                $data = array('status' => 1, 'message' => 'Done');
+            }
+
+        } catch (\Exception $e) {
+            $data = array('status' => 0, 'message' => 'Error');
+        }
+
+        $response = new JsonResponse();
+        $response->setData($data);
+
+        return $response;
     }
 
 }
