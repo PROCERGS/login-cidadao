44f19ddad7ffcc1a1a6440bed44c272a588e4cb5
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-05-05 19:04:17 -0300

    Organizing views

---
+++ src/PROCERGS/LoginCidadao/UserBundle/Entity/PersonRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PROCERGS\LoginCidadao\CoreBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class PersonRepository extends EntityRepository
+{
+
+    public function findAllPendingCPF()
+    {
+        return $this->getEntityManager()
+                        ->createQuery('SELECT p FROM PROCERGSLoginCidadaoCoreBundle:Person p WHERE p.cpf IS NULL')
+                        ->getResult();
+    }
+
+    public function findAllPendingCPFUntilDate($date)
+    {
+        return $this->getEntityManager()
+                        ->createQuery('SELECT p FROM PROCERGSLoginCidadaoCoreBundle:Person p WHERE p.cpf IS NULL AND p.cpfExpiration >= :date')
+                        ->setParameter('date', $date)
+                        ->getResult();
+    }
+
+    public function findUnconfirmedEmailUntilDate(\DateTime $dateLimit)
+    {
+        return $this->getEntityManager()
+                        ->createQuery('SELECT p FROM PROCERGSLoginCidadaoCoreBundle:Person p WHERE p.confirmationToken IS NOT NULL AND p.emailExpiration <= :date')
+                        ->setParameter('date', $dateLimit)
+                        ->getResult();
+    }
+
+}
