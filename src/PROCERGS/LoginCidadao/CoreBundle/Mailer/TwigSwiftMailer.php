d397bae892fead4b4cc97acd8b0b6503b5bd3eea
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-05-06 18:07:39 -0300

    Added email sender name

--- src/PROCERGS/LoginCidadao/CoreBundle/Mailer/TwigSwiftMailer.php
+++ src/PROCERGS/LoginCidadao/CoreBundle/Mailer/TwigSwiftMailer.php
@@ -10,12 +10,15 @@ class TwigSwiftMailer extends BaseMailer
     public function sendEmailChangedMessage(UserInterface $user, $oldEmail)
     {
         $template = $this->parameters['template']['email_changed'];
+        $fromEmail = $this->parameters['from_email']['email_changed'];
+        $fromName = $this->parameters['from_email']['email_sender_name'];
+        $from = array($fromEmail => $fromName);
 
         $context = array(
             'user' => $user,
             'oldEmail' => $oldEmail
         );
 
-        $this->sendMessage($template, $context, $this->parameters['from_email']['email_changed'], $oldEmail);
+        $this->sendMessage($template, $context, $from, $oldEmail);
     }
 }
