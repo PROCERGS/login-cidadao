7bad1ead861da72aa2b258030f229be7e9e16e82
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-05-26 12:39:30 -0300

    Switched to FOSRestBundle to handle API calls

--- src/PROCERGS/LoginCidadao/APIBundle/Controller/PersonController.php
+++ src/PROCERGS/LoginCidadao/APIBundle/Controller/PersonController.php
@@ -2,80 +2,62 @@
 
 namespace PROCERGS\LoginCidadao\APIBundle\Controller;
 
+use FOS\RestBundle\Controller\Annotations as REST;
+use FOS\RestBundle\Controller\FOSRestController;
 use JMS\Serializer\SerializationContext;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use PROCERGS\LoginCidadao\APIBundle\Exception\RequestTimeoutException;
-use OAuth2\OAuth2ServerException;
+use PROCERGS\LoginCidadao\CoreBundle\Entity\Person;
+use PROCERGS\LoginCidadao\CoreBundle\Entity\Authorization;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-class PersonController extends Controller
+class PersonController extends FOSRestController
 {
 
     /**
-     * @Route("/person")
-     * @Template()
+     * @REST\Get("/person")
+     * @REST\View
      */
     public function selfAction()
     {
-        $user = $this->getUser();
-        $scope = $this->getClientScope($user);
+        $person = $this->getUser();
+        $scope = $this->getClientScope($person);
 
-        $json = $this->serializePerson($user, $scope);
-
-        $response = new JsonResponse();
-        $response->setData(json_decode($json));
-        return $response;
+        $view = $this->view($this->preparePerson($person))
+                ->setSerializationContext($this->getSerializationContext($scope));
+        return $this->handleView($view);
     }
 
     /**
-     * @Route("/person/wait/update")
-     * @Template()
+     * @REST\Get("/wait/person/update")
+     * @REST\View
      */
-    public function waitVoterRegistrationAction()
+    public function waitPersonChangeAction()
     {
         $user = $this->getUser();
         $scope = $this->getClientScope($user);
-        $updatedAt = date_create($this->getRequest()->get('updated_at'));
-        if (!$scope || !$updatedAt) {
-            $e = new OAuth2ServerException(403, 'Access Denied');
-            return $e->getHttpResponse();
+        $updatedAt = \DateTime::createFromFormat('Y-m-d H:i:s',
+                        $this->getRequest()->get('updated_at'));
+
+        if (!($updatedAt instanceof \DateTime)) {
+            throw new BadRequestHttpException("Missing or incorrect format for minimum update time. Please use Y-m-d H:i:s");
         }
-        $people = $this->getDoctrine()->getManager()->getRepository('PROCERGSLoginCidadaoCoreBundle:Person');
+
         $id = $user->getId();
         $lastUpdatedAt = null;
-        $callback = function() use ($id, $people, $updatedAt, &$lastUpdatedAt) {
-            $person = $people->find($id);
-            if (!$person->getUpdatedAt()) {
-                return false;
-            }
-            if ($person->getUpdatedAt() > $updatedAt) {
-                return $person;
-            } else {
-                if ($lastUpdatedAt === null) {
-                    $lastUpdatedAt = $person->getUpdatedAt();
-                } elseif ($person->getUpdatedAt() != $lastUpdatedAt) {
-                    return $person;
-                }
-            }
-            return false;
-        };
-        try {
-            $person = $this->runTimeLimited($callback);
-        } catch (RequestTimeoutException $e) {
-            $e = new OAuth2ServerException('408', 'Request Timeout');
-            return $e->getHttpResponse();
-        }
-        $json = $this->serializePerson($person, $scope);
-
-        return new JsonResponse(json_decode($json));
+        $callback = $this->getCheckUpdateCallback($id, $updatedAt,
+                $lastUpdatedAt);
+        $person = $this->runTimeLimited($callback);
+        $context = SerializationContext::create()->setGroups($scope);
+        $view = $this->view($this->preparePerson($person))
+                ->setSerializationContext($context);
+        return $this->handleView($view);
     }
 
     private function runTimeLimited($callback, $waitTime = 1)
     {
-        $limit = ini_get('max_execution_time') ? ini_get('max_execution_time') - 2 : 60;
+        $maxExecutionTime = ini_get('max_execution_time');
+        $limit = $maxExecutionTime ? $maxExecutionTime - 2 : 60;
         $startTime = time();
         while ($limit > 0) {
             $result = call_user_func($callback);
@@ -83,24 +65,32 @@ class PersonController extends Controller
 
             if ($result !== false) {
                 return $result;
-            } else {
-                $limit -= $delta;
-                if ($limit <= 0) {
-                    break;
-                }
-                $startTime = time();
-                sleep($waitTime);
             }
+
+            $limit -= $delta;
+            if ($limit <= 0) {
+                break;
+            }
+            $startTime = time();
+            sleep($waitTime);
         }
-        throw new RequestTimeoutException();
+        throw new RequestTimeoutException("Request Timeout");
     }
 
-    private function serializePerson($person, $scope)
+    private function preparePerson(Person $person)
     {
         $imgHelper = $this->container->get('vich_uploader.templating.helper.uploader_helper');
         $templateHelper = $this->get('templating.helper.assets');
         $isDev = $this->get('kernel')->getEnvironment() === 'dev';
-        $person->prepareAPISerialize($imgHelper, $templateHelper, $isDev, $this->getRequest());
+        $person->prepareAPISerialize($imgHelper, $templateHelper, $isDev,
+                $this->getRequest());
+
+        return $person;
+    }
+
+    private function serializePerson($person, $scope)
+    {
+        $person = $this->preparePerson($person, $scope);
 
         $serializer = $this->container->get('jms_serializer');
         return $serializer->serialize($person, 'json',
@@ -119,7 +109,40 @@ class PersonController extends Controller
             'person' => $user,
             'client' => $client
         ));
+        if (!($authorization instanceof Authorization)) {
+            throw new AccessDeniedException();
+        }
+
         return $authorization->getScope();
+    }
+
+    private function getCheckUpdateCallback($id, $updatedAt, $lastUpdatedAt)
+    {
+        $people = $this->getDoctrine()->getManager()
+                ->getRepository('PROCERGSLoginCidadaoCoreBundle:Person');
+        return function() use ($id, $people, $updatedAt, $lastUpdatedAt) {
+            $person = $people->find($id);
+            if (!$person->getUpdatedAt()) {
+                return false;
+            }
+
+            if ($person->getUpdatedAt() > $updatedAt) {
+                return $person;
+            }
+
+            if ($lastUpdatedAt === null) {
+                $lastUpdatedAt = $person->getUpdatedAt();
+            } elseif ($person->getUpdatedAt() != $lastUpdatedAt) {
+                return $person;
+            }
+
+            return false;
+        };
+    }
+
+    private function getSerializationContext($scope)
+    {
+        return SerializationContext::create()->setGroups($scope);
     }
 
 }
