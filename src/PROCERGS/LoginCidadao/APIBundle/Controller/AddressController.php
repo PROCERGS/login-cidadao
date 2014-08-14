c42f4c295dbef2fbbcda0898f5dfe3ca4d93025a
lordvacilaoerrocerto <root@localhost>
2014-08-14 16:44:43 -0300

    lets try

--- src/PROCERGS/LoginCidadao/APIBundle/Controller/AddressController.php
+++ src/PROCERGS/LoginCidadao/APIBundle/Controller/AddressController.php
@@ -5,6 +5,7 @@ namespace PROCERGS\LoginCidadao\APIBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use FOS\RestBundle\Controller\Annotations as REST;
+use Symfony\Component\HttpFoundation\Request;
 
 class AddressController extends FOSRestController
 {
@@ -32,6 +33,68 @@ class AddressController extends FOSRestController
 
         $view = $this->view($result);
         return $this->handleView($view);
+    }
+    
+    /**
+     * @REST\Get("/public/country", name="lc_search_country" )
+     * @REST\View()
+     */
+    public function searchCountryAction()
+    {
+        $request = $this->getRequest();
+        $query = $this->getDoctrine()->getRepository('PROCERGSLoginCidadaoCoreBundle:Country')
+        ->createQueryBuilder('cty')
+        ->where('cty.reviewed = ' . Country::REVIEWED_OK)
+        ->orderBy('cty.id', 'ASC');
+        return $this->handleView($this->view($query->getQuery()->getResult(Doctrine\ORM\Query::HYDRATE_ARRAY)));
+    }
+    
+    /**
+     * @REST\Get("/public/uf", name="lc_search_uf" )
+     * @REST\View()
+     */
+    public function searchUfAction(Request $request = null)
+    {
+        $request = $this->getRequest();
+        $query = $this->getDoctrine()->getRepository('PROCERGSLoginCidadaoCoreBundle:Uf')
+        ->createQueryBuilder('uf')
+        ->where('uf.reviewed = ' . Country::REVIEWED_OK);
+        $country = $request->get('country_id');
+        if ($country) {
+            $query->join('PROCERGSLoginCidadaoCoreBundle:Country', 'cty', 'WITH', 'uf.country = cty');
+            $query->andWhere('cty.id = :country');
+            $query->setParameter('country', $country);
+        }
+        $query->orderBy('uf.id', 'ASC');
+        return $this->handleView($this->view($query->getQuery()->getResult(Doctrine\ORM\Query::HYDRATE_ARRAY)));
+    }
+    
+    /**
+     * @REST\Get("/public/city", name="lc_search_city" )
+     * @REST\View()
+     */
+    public function searchCityAction(Request $request = null)
+    {
+        $request = $this->getRequest();
+        $result = $this->getDoctrine()->getRepository('PROCERGSLoginCidadaoCoreBundle:City')
+        ->createQueryBuilder('c')
+        ->where('c.reviewed = ' . Country::REVIEWED_OK);
+        $country = $request->get('country_id');
+        $uf = $request->get('uf_id');
+        if ($country || $uf) {
+            $query->join('PROCERGSLoginCidadaoCoreBundle:Uf', 'uf', 'WITH', 'c.uf = uf');
+        }
+        if ($country) {
+            $query->join('PROCERGSLoginCidadaoCoreBundle:Country', 'cty', 'WITH', 'uf.country = cty');
+            $query->andWhere('cty.id = :country');
+            $query->setParameter('country', $country);
+        }
+        if ($uf) {
+            $query->andWhere('uf.id = :uf');
+            $query->setParameter('uf', $uf);
+        }
+        $query->orderBy('c.id', 'ASC');
+        return $this->handleView($this->view($query->getQuery()->getResult(Doctrine\ORM\Query::HYDRATE_ARRAY)));
     }
 
 }
