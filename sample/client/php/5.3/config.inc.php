2d763af4d8517f20a380aea32269b9604e133966
lordvacilaoerrocerto <root@localhost>
2014-09-12 17:00:32 -0300

    moved samples to examples branch

--- sample/client/php/5.3/config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-    const siteUrl = "http://myapp.localhost";
-    const apiUri = "https://meu.rs.gov.br/api/v1/person";
-    const client_id = "";
-    const client_secret = "";
-    const authorize_endpoint = "https://meu.rs.gov.br/oauth/v2/auth";
-    const token_endpoint = "https://meu.rs.gov.br/oauth/v2/token";
-    const redirect_uri = "http://myapp.localhost/callback.php";
-?>
