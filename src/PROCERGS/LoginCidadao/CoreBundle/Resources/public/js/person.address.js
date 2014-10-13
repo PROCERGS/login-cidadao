d3c5602d44d14c47300ffa260ba8ad728b7b6049
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-10-13 14:43:09 -0300

    Switched from deprecated {% include %} to {{ include() }}

--- src/PROCERGS/LoginCidadao/CoreBundle/Resources/public/js/person.address.js
+++ src/PROCERGS/LoginCidadao/CoreBundle/Resources/public/js/person.address.js
@@ -7,3 +7,28 @@ $(document).ready(function () {
         $(this).closest('.remove-box').slideUp();
     });
 });
+
+if ($('.city-selector').length) {
+    var cities = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: cityTypeaheadSearchUrl,
+        prefetch: cityTypeaheadPrefetchUrl,
+    });
+    cities.initialize();
+    $('.city-selector').typeahead(null, {
+        name: 'city',
+        displayKey: function (city) {
+            return city.name + ', ' + city.state.acronym;
+        },
+        source: cities.ttAdapter(),
+        templates: {
+            empty: [
+                '<div class="empty-message">',
+                'No city found.',
+                '</div>'
+            ].join('\n'),
+            suggestion: Handlebars.compile('<p><strong>{\{name}}</strong>, {\{state.acronym}}</p>')
+        }
+    });
+}
