5c813e4fa3993fac7d93e99f2b36860f01abaabb
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-10-12 21:54:06 -0600

    Added address removal

--- src/PROCERGS/LoginCidadao/CoreBundle/Controller/PersonAddressController.php
+++ src/PROCERGS/LoginCidadao/CoreBundle/Controller/PersonAddressController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use PROCERGS\LoginCidadao\CoreBundle\Entity\PersonAddress;
+use PROCERGS\LoginCidadao\CoreBundle\Form\Type\RemovePersonAddressFormType;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class PersonAddressController extends Controller
 {
@@ -19,7 +21,12 @@ class PersonAddressController extends Controller
     {
         $person = $this->getUser();
 
-        return compact('person');
+        foreach ($person->getAddresses() as $address) {
+            $data = array('address_id' => $address->getId());
+            $deleteForms[$address->getId()] = $this->createForm(new RemovePersonAddressFormType(), $data)->createView();
+        }
+
+        return compact('person', 'deleteForms');
     }
 
     /**
@@ -47,12 +54,38 @@ class PersonAddressController extends Controller
     }
 
     /**
-     * @ Route("/person/addresses/new", name="lc_person_addresses_new")
+     * @Route("/person/addresses/{id}/remove", name="lc_person_addresses_delete")
      * @Template()
      */
-    public function deleteAction()
+    public function deleteAction($id)
     {
+        $translator = $this->get('translator');
+        $form = $this->createForm(new RemovePersonAddressFormType());
+        $form->handleRequest($this->getRequest());
 
+        if ($form->isValid()) {
+            $person = $this->getUser();
+            $em = $this->getDoctrine()->getManager();
+            $addresses = $em->getRepository('PROCERGSLoginCidadaoCoreBundle:PersonAddress');
+            $address = $addresses->find($id);
+
+            try {
+                if ($address->getPerson()->getId() !== $person->getId()) {
+                    throw new AccessDeniedException();
+                }
+                $em->remove($address);
+                $em->flush();
+            } catch (AccessDeniedException $e) {
+                $this->get('session')->getFlashBag()->add('error', $translator->trans("Access Denied."));
+            } catch (\Exception $e) {
+                $this->get('session')->getFlashBag()->add('error', $translator->trans("Wasn't possible to remove this address."));
+                $this->get('session')->getFlashBag()->add('error', $e->getMessage());
+            }
+        } else {
+            $this->get('session')->getFlashBag()->add('error', $translator->trans("Wasn't possible to remove this address."));
+        }
+
+        return $this->redirect($this->generateUrl('lc_person_addresses'));
     }
 
 }
