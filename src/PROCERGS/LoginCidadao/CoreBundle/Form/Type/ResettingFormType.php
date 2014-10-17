a4dbbc56c618fd70c1fdb1cbd997b4c29c4016bc
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-10-17 16:07:04 -0300

    Updated forms to use global theme

--- src/PROCERGS/LoginCidadao/CoreBundle/Form/Type/ResettingFormType.php
+++ src/PROCERGS/LoginCidadao/CoreBundle/Form/Type/ResettingFormType.php
@@ -7,18 +7,19 @@ use FOS\UserBundle\Form\Type\ResettingFormType as BaseType;
 
 class ResettingFormType extends BaseType
 {
-    
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);
-        
+
         $builder->add('plainPassword', 'repeated', array(
             'type' => 'password',
-            'options' => array('attr' => array('class' => 'form-control'), 'translation_domain' => 'FOSUserBundle'),
+            'options' => array('translation_domain' => 'FOSUserBundle'),
             'first_options' => array('label' => 'form.new_password'),
             'second_options' => array('label' => 'form.new_password_confirmation'),
             'invalid_message' => 'fos_user.password.mismatch',
-        ));
+        ))
+        ->add('save', 'submit');
     }
 
     public function getName()
