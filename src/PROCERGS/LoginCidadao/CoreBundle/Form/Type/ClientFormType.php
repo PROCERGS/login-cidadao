31d669cd585425cf2b4b747170b15ea425b2d34d
Guilherme Donato <guilhermednt@users.noreply.github.com>
2015-01-30 18:20:59 -0200

    Removed comments

--- src/PROCERGS/LoginCidadao/CoreBundle/Form/Type/ClientFormType.php
+++ src/PROCERGS/LoginCidadao/CoreBundle/Form/Type/ClientFormType.php
@@ -20,36 +20,6 @@ class ClientFormType extends ClientBaseFormType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);
-        /*
-        $a = explode(' ', $this->lcScope);
-        $a = array_combine($a, $a);
-        $builder->add('allowedScopes', 'collection', array(
-            'type' => 'choice',
-            'options' => array(
-                'choices' => $a
-            ),
-            'required' => true,
-            'allow_add' => true
-        ));
-        $builder->add('allowedGrantTypes', 'collection', array(
-            'type' => 'choice',
-            'options' => array(
-                'choices' => array(
-                    OAuth2::GRANT_TYPE_AUTH_CODE => 'authorization_code',
-                    OAuth2::GRANT_TYPE_IMPLICIT => 'token',
-                    OAuth2::GRANT_TYPE_USER_CREDENTIALS => 'password',
-                    OAuth2::GRANT_TYPE_CLIENT_CREDENTIALS => 'client_credentials',
-                    OAuth2::GRANT_TYPE_REFRESH_TOKEN => 'refresh_token',
-                    OAuth2::GRANT_TYPE_EXTENSIONS => 'extensions'
-                ),
-                'preferred_choices' => array(
-                    OAuth2::GRANT_TYPE_AUTH_CODE
-                )
-            ),
-            'required' => true,
-            'allow_add' => true
-        ));
-        */
     }
 
     public function getName()
