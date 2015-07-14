00063c16da9c00fd011cd596634529fc3671942c
Guilherme Donato <guilhermednt@users.noreply.github.com>
2015-07-14 10:38:26 -0300

    Fixes #222

--- src/PROCERGS/LoginCidadao/CoreBundle/EventListener/ProfileEditListner.php
+++ src/PROCERGS/LoginCidadao/CoreBundle/EventListener/ProfileEditListner.php
@@ -367,6 +367,7 @@ class ProfileEditListner implements EventSubscriberInterface
                 $state->setName($stateText);
                 $state->setCountry($data->getCountry());
                 $this->em->persist($state);
+                $this->em->flush($state);
             }
             $data->setState($state)
                 ->setCity(null);
