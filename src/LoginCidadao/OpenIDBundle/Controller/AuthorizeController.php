98f32e0b5657392d2efcf9eb70feb081b9e2e0f7
Guilherme Donato <guilhermednt@users.noreply.github.com>
2015-09-23 18:09:10 -0300

    Using login-cidadao style for authorization page. #235

--- src/LoginCidadao/OpenIDBundle/Controller/AuthorizeController.php
+++ src/LoginCidadao/OpenIDBundle/Controller/AuthorizeController.php
@@ -5,6 +5,7 @@ namespace LoginCidadao\OpenIDBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class AuthorizeController extends Controller
 {
@@ -19,5 +20,28 @@ class AuthorizeController extends Controller
 
         return $server->handleAuthorizeRequest($this->get('oauth2.request'),
                 $this->get('oauth2.response'), true, $this->getUser()->getId());
+    }
+
+    /**
+     * @Template()
+     */
+    public function authorizeAction($client_id, $scope, $response_type,
+                                    $redirect_uri, $state = null, $nonce = null)
+    {
+        $id     = explode('_', $client_id);
+        $em     = $this->getDoctrine()->getManager();
+        $client = $em->getRepository('PROCERGSOAuthBundle:Client')
+            ->find($id[0]);
+
+
+        $scopes       = array();
+        $scopeStorage = $this->get('oauth2.storage.scope');
+        foreach (explode(' ', $scope) as $scope) {
+            $scopes[] = $scopeStorage->getDescriptionForScope($scope);
+        }
+
+        $qs = compact('client_id', 'scope', 'response_type', 'redirect_uri',
+            'state', 'nonce');
+        return compact('qs', 'scopes', 'client');
     }
 }
