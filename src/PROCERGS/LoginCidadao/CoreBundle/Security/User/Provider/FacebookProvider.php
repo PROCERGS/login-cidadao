7619beea3a5deafadc376060bfeb91d93b7a7170
Guilherme Donato <guilhermednt@users.noreply.github.com>
2015-10-04 22:37:40 -0300

    FacebookProvider now complies to Graph API 2.0. fixes #257

--- src/PROCERGS/LoginCidadao/CoreBundle/Security/User/Provider/FacebookProvider.php
+++ src/PROCERGS/LoginCidadao/CoreBundle/Security/User/Provider/FacebookProvider.php
@@ -20,7 +20,6 @@ use PROCERGS\LoginCidadao\CoreBundle\Exception\LcFcGbException;
 
 class FacebookProvider implements UserProviderInterface
 {
-
     /**
      * @var \Facebook
      */
@@ -44,19 +43,19 @@ class FacebookProvider implements UserProviderInterface
 
         // Proxy Settings
         try {
-            $proxy = $container->getParameter('http_proxy');
-            Facebook::$CURL_OPTS[CURLOPT_PROXYTYPE] = $proxy['type'];
-            Facebook::$CURL_OPTS[CURLOPT_PROXY] = $proxy['host'];
-            Facebook::$CURL_OPTS[CURLOPT_PROXYPORT] = $proxy['port'];
+            $proxy                                     = $container->getParameter('http_proxy');
+            Facebook::$CURL_OPTS[CURLOPT_PROXYTYPE]    = $proxy['type'];
+            Facebook::$CURL_OPTS[CURLOPT_PROXY]        = $proxy['host'];
+            Facebook::$CURL_OPTS[CURLOPT_PROXYPORT]    = $proxy['port'];
             Facebook::$CURL_OPTS[CURLOPT_PROXYUSERPWD] = $proxy['auth'];
         } catch (ParameterNotFoundException $e) {
             // no proxy
         }
 
         $this->userManager = $userManager;
-        $this->validator = $validator;
-        $this->container = $container;
-        $this->dispatcher = $dispatcher;
+        $this->validator   = $validator;
+        $this->container   = $container;
+        $this->dispatcher  = $dispatcher;
         $this->formFactory = $formFactory;
     }
 
@@ -79,7 +78,7 @@ class FacebookProvider implements UserProviderInterface
     {
 
         try {
-            $fbdata = $this->facebook->api('/me');
+            $fbdata = $this->facebook->api('/me?fields=id,name,first_name,last_name,email,age_range,birthday');
         } catch (FacebookApiException $e) {
             $fbdata = null;
             return false;
@@ -126,23 +125,23 @@ class FacebookProvider implements UserProviderInterface
         }
 
         $newUser = false;
-        $user = $this->findUserByFbId($username);
+        $user    = $this->findUserByFbId($username);
 
-        if(($user && $currentUserObj)){
-            if($user->getFacebookId() != $currentUserObj->getFacebookId()){
+        if (($user && $currentUserObj)) {
+            if ($user->getFacebookId() != $currentUserObj->getFacebookId()) {
                 throw new AlreadyLinkedAccount();
             }
         }
 
         try {
-            $fbdata = $this->facebook->api('/me');
+            $fbdata = $this->facebook->api('/me?fields=id,name,first_name,last_name,email,age_range,birthday');
         } catch (FacebookApiException $e) {
             switch ($e->getType()) {
-            	case 'OAuthException':
-            	case 'invalid_token':
-            	case 'Exception':
-            	    throw new LcFcGbException();
-            	    break;
+                case 'OAuthException':
+                case 'invalid_token':
+                case 'Exception':
+                    throw new LcFcGbException();
+                    break;
             }
             $fbdata = null;
         }
@@ -150,7 +149,7 @@ class FacebookProvider implements UserProviderInterface
             if (empty($user)) {
                 if (!($currentUserObj instanceof UserInterface)) {
                     $newUser = true;
-                    $user = $this->userManager->createUser();
+                    $user    = $this->userManager->createUser();
                     $user->setEnabled(true);
                     $user->setPassword('');
                     $user->setEmailConfirmedAt(new \DateTime());
@@ -161,9 +160,8 @@ class FacebookProvider implements UserProviderInterface
             }
 
             if ($user->getUsername() == '' || $user->getUsername() == null) {
-                $defaultUsername = $username . '@facebook.com';
-                $availUsername = $this->userManager->getNextAvailableUsername((isset($fbdata['username']) ? $fbdata['username'] : $username),
-                        10, $defaultUsername);
+                $availUsername = $this->prepareFacebookUsername($username,
+                    $fbdata);
                 $user->setUsername($availUsername);
             }
 
@@ -178,20 +176,19 @@ class FacebookProvider implements UserProviderInterface
             $form = $this->formFactory->createForm();
             $form->setData($user);
 
-            $request = $this->container->get('request');
+            $request       = $this->container->get('request');
             $eventResponse = new \Symfony\Component\HttpFoundation\RedirectResponse('/');
-            $event = new FormEvent($form, $request);
+            $event         = new FormEvent($form, $request);
             if ($newUser) {
                 $this->dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS,
-                        $event);
+                    $event);
             }
 
             $this->userManager->updateUser($user);
 
             if ($newUser) {
                 $this->dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED,
-                        new FilterUserResponseEvent($user, $request,
-                        $eventResponse));
+                    new FilterUserResponseEvent($user, $request, $eventResponse));
             }
         }
 
@@ -207,10 +204,32 @@ class FacebookProvider implements UserProviderInterface
     {
         if (!$this->supportsClass(get_class($user)) || !$user->getFacebookId()) {
             throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.',
-                    get_class($user)));
+                get_class($user)));
         }
 
         return $this->loadUserByUsername($user->getFacebookId());
     }
 
+    private function prepareFacebookUsername($username, $fbdata)
+    {
+        $defaultUsername      = $username.'@facebook.com';
+        $fbUsernameComponents = array();
+        if (isset($fbdata['first_name'])) {
+            $fbUsernameComponents[] = strtolower($fbdata['first_name']);
+        }
+        if (isset($fbdata['last_name'])) {
+            $fbUsernameComponents[] = strtolower($fbdata['last_name']);
+        }
+
+        if (count($fbUsernameComponents) > 0) {
+            $fbUsername = implode('.', $fbUsernameComponents);
+        } else {
+            $fbUsername = $username;
+        }
+
+        $availUsername = $this->userManager->getNextAvailableUsername((isset($fbdata['username'])
+                    ? $fbdata['username'] : $fbUsername), 10, $defaultUsername);
+
+        return $availUsername;
+    }
 }
