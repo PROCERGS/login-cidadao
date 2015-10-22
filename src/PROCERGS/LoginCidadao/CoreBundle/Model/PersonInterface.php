a0844db08c2172d247290ef32e82f303942a7135
Guilherme Donato <guilhermednt@users.noreply.github.com>
2015-10-22 19:40:23 -0200

    Fixed typo

--- src/PROCERGS/LoginCidadao/CoreBundle/Model/PersonInterface.php
+++ src/PROCERGS/LoginCidadao/CoreBundle/Model/PersonInterface.php
@@ -155,7 +155,7 @@ interface PersonInterface
      */
     public function getImageName();
 
-    public function setProfilePictureUrl($profilePicutreUrl);
+    public function setProfilePictureUrl($profilePictureUrl);
 
     public function getProfilePictureUrl();
 
