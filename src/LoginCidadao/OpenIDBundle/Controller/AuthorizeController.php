d6fb16392ddbcdb8c256ab69c73150bc999ae5a2
Guilherme Donato <guilhermednt@users.noreply.github.com>
2015-10-22 17:29:31 -0200

    Skipping authorization screen when already authorized fixes #271

--- src/LoginCidadao/OpenIDBundle/Controller/AuthorizeController.php
+++ src/LoginCidadao/OpenIDBundle/Controller/AuthorizeController.php
@@ -4,20 +4,21 @@ namespace LoginCidadao\OpenIDBundle\Controller;
 
 use FOS\OAuthServerBundle\Event\OAuthEvent;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use OAuth2\ServerBundle\Controller\AuthorizeController as BaseController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class AuthorizeController extends Controller
+class AuthorizeController extends BaseController
 {
 
     /**
      * @Route("/openid/connect/authorize", name="_authorize_handle")
      * @Method({"POST"})
      */
-    public function handleAuthorizeAction(Request $request)
+    public function handleAuthorizeAction()
     {
+        $request       = $this->getRequest();
         $scope         = $request->request->get('scope');
         $is_authorized = $request->request->has('rejected') === false || $request->request->has('accepted')
             === true;
@@ -25,9 +26,7 @@ class AuthorizeController extends Controller
 
         $server = $this->get('oauth2.server');
 
-        $response = $server->handleAuthorizeRequest($this->get('oauth2.request'),
-            $this->get('oauth2.response'), $is_authorized,
-            $this->getUser()->getId());
+        $response = $this->handleAuthorize($server, $is_authorized);
 
         $id     = explode('_', $request->get('client_id'));
         $em     = $this->getDoctrine()->getManager();
@@ -71,5 +70,37 @@ class AuthorizeController extends Controller
     private function getScopeManager()
     {
         return $this->get('oauth2.scope_manager');
+    }
+
+    /**
+     * @Route("/openid/connect/authorize", name="_authorize_validate")
+     * @Method({"GET"})
+     * @Template("OAuth2ServerBundle:Authorize:authorize.html.twig")
+     */
+    public function validateAuthorizeAction()
+    {
+        $request = $this->getRequest();
+        $id      = explode('_', $request->get('client_id'));
+        $em      = $this->getDoctrine()->getManager();
+        $client  = $em->getRepository('PROCERGSOAuthBundle:Client')->find($id[0]);
+
+        $event = $this->get('event_dispatcher')->dispatch(
+            OAuthEvent::PRE_AUTHORIZATION_PROCESS,
+            new OAuthEvent($this->getUser(), $client)
+        );
+
+        $server = $this->get('oauth2.server');
+        if ($event->isAuthorizedClient()) {
+            return $this->handleAuthorize($server, $event->isAuthorizedClient());
+        }
+
+        return parent::validateAuthorizeAction();
+    }
+
+    private function handleAuthorize($server, $is_authorized)
+    {
+        return $server->handleAuthorizeRequest($this->get('oauth2.request'),
+                $this->get('oauth2.response'), $is_authorized,
+                $this->getUser()->getId());
     }
 }
