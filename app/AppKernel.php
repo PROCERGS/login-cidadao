4193a78c80275ddc6904e52149159da522fc83e5
Guilherme Donato <github@cadastro.dona.to>
2015-12-12 21:37:18 -0200

    Applying PROCERGS changes

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -57,6 +57,9 @@ class AppKernel extends Kernel
 
             new Nelmio\CorsBundle\NelmioCorsBundle(),
             new Nelmio\SecurityBundle\NelmioSecurityBundle(),
+
+            // PROCERGS
+            new PROCERGS\LoginCidadao\CoreBundle\PROCERGSLoginCidadaoCoreBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
