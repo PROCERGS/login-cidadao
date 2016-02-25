2ac77f07caac12e675f947bacb88ea0910d364d2
Guilherme Donato <guilhermednt@users.noreply.github.com>
2016-02-25 17:11:47 -0300

    added redirect_url support in remote logout

--- src/LoginCidadao/CoreBundle/Controller/DefaultController.php
+++ src/LoginCidadao/CoreBundle/Controller/DefaultController.php
@@ -12,6 +12,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use LoginCidadao\CoreBundle\Entity\SentEmail;
 use LoginCidadao\APIBundle\Entity\LogoutKey;
+use League\Uri\Schemes\Http as HttpUri;
 
 class DefaultController extends Controller
 {
@@ -95,33 +96,6 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/logout/if-not-remembered", name="lc_logout_not_remembered")
-     * @Template()
-     */
-    public function logoutIfNotRememberedAction(Request $request)
-    {
-        $result['logged_out'] = false;
-        if ($this->getUser() instanceof UserInterface) {
-            if ($request->cookies->has('REMEMBERME')) {
-                $result = array('logged_out' => false);
-            } else {
-                $this->get("request")->getSession()->invalidate();
-                $this->get("security.context")->setToken(null);
-                $result['logged_out'] = true;
-            }
-        } else {
-            $result['logged_out'] = true;
-        }
-
-        $response  = new JsonResponse();
-        $userAgent = $request->headers->get('User-Agent');
-        if (preg_match('/(?i)msie [1-9]/', $userAgent)) {
-            $response->headers->set('Content-Type', 'text/json');
-        }
-        return $response->setData($result);
-    }
-
-    /**
      * @Route("/dashboard", name="lc_dashboard")
      * @Template()
      */
@@ -184,8 +158,19 @@ class DefaultController extends Controller
             $response->headers->set('Content-Type', 'text/json');
         }
 
+        $client = $logoutKey->getClient();
         $em->remove($logoutKey);
         $em->flush();
+
+        $redirectUrl = $request->get('redirect_url');
+        if ($redirectUrl !== null) {
+            $uri = HttpUri::createFromString($redirectUrl);
+            if ($client->ownsDomain($uri->getHost())) {
+                return $this->redirect($redirectUrl);
+            } else {
+                $result['error'] = "Invalid redirect_url domain. It doesn't appear to belong to {$client->getName()}";
+            }
+        }
 
         return $response->setData($result);
     }
