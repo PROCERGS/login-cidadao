34236d61f73ceb31f51c0372c79fe3d0fb77f6ba
Guilherme Donato <guilhermednt@users.noreply.github.com>
2016-07-20 15:05:24 -0300

    improved password validation #409

--- src/LoginCidadao/CoreBundle/Entity/Person.php
+++ src/LoginCidadao/CoreBundle/Entity/Person.php
@@ -97,7 +97,9 @@ class Person extends BaseUser implements PersonInterface, TwoFactorInterface, Ba
      * )
      * @Assert\Length(
      *     min=8,
-     *     max=4096,
+     *     max=72,
+     *     maxMessage="person.validation.password.length.max",
+     *     minMessage="person.validation.password.length.min",
      *     groups={"Registration", "ResetPassword", "ChangePassword", "LoginCidadaoRegistration"}
      * )
      */
