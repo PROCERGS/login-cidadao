20aabb991ed466bf5bd04ee30aa38c2776cd5d11
Guilherme Donato <guilhermednt@users.noreply.github.com>
2016-08-01 18:01:16 -0300

    persisting changes #423

--- src/LoginCidadao/CoreBundle/Controller/TaskController.php
+++ src/LoginCidadao/CoreBundle/Controller/TaskController.php
@@ -17,6 +17,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class TaskController extends Controller
 {
@@ -55,18 +56,20 @@ class TaskController extends Controller
         $response = null;
         $form->handleRequest($request);
         if ($form->isValid()) {
-            $event = new FormEvent($person, $request);
+            /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+            $userManager = $this->get('fos_user.user_manager');
+
+            $event = new FormEvent($form, $request);
             $dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_SUCCESS, $event);
 
-            // TODO: persist
+            $userManager->updateUser($person);
 
-            $response = $this->render(
-                'LoginCidadaoCoreBundle:Task:confirmEmail.html.twig',
-                ['targetUrl' => $targetUrl, 'form' => $form->createView()]
-            );
+            $response = $this->redirectToRoute('task_confirm_email');
             $event = new FilterUserResponseEvent($person, $request, $response);
             $dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_COMPLETED, $event);
             $response = $event->getResponse();
+
+            $this->flashEmailSent($request->getSession());
         }
 
         if ($resend) {
@@ -83,7 +86,6 @@ class TaskController extends Controller
     private function resendEmailConfirmation(SessionInterface $session, PersonInterface $person)
     {
         $mailer = $this->get('fos_user.mailer');
-        $translator = $this->get('translator');
 
         if (is_null($person->getEmailConfirmedAt())) {
             if (is_null($person->getConfirmationToken())) {
@@ -94,14 +96,21 @@ class TaskController extends Controller
             }
             $mailer->sendConfirmationEmailMessage($person);
 
-            if ($session instanceof Session) {
-                $session->getFlashBag()->add(
-                    'success',
-                    $translator->trans("tasks.confirm_email.resent.alert")
-                );
-            }
+            $this->flashEmailSent($session);
 
             return $this->redirectToRoute('task_confirm_email');
+        }
+    }
+
+    private function flashEmailSent(SessionInterface $session)
+    {
+        if ($session instanceof Session) {
+            /** @var TranslatorInterface $translator */
+            $translator = $this->get('translator');
+            $session->getFlashBag()->add(
+                'success',
+                $translator->trans("tasks.confirm_email.resent.alert")
+            );
         }
     }
 }
