d657a6b28d33de888ae8141b266c0ddfe893e572
Guilherme Donato <guilhermednt@users.noreply.github.com>
2016-08-04 16:26:01 -0300

    work in progress #429

--- src/LoginCidadao/OpenIDBundle/Controller/SessionManagementController.php
+++ src/LoginCidadao/OpenIDBundle/Controller/SessionManagementController.php
@@ -10,6 +10,7 @@
 
 namespace LoginCidadao\OpenIDBundle\Controller;
 
+use LoginCidadao\OpenIDBundle\Storage\PublicKey;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -88,7 +89,41 @@ class SessionManagementController extends Controller
      */
     public function endSessionAction(Request $request)
     {
-        //
+        $idToken = $request->get('id_token_hint');
+        $postLogoutUri = $request->get('post_logout_redirect_uris', null);
+
+        $getConsent = true;
+        if ($idToken) {
+            if ($this->checkIdToken($idToken)) {
+                $getConsent = false;
+            } else {
+                // TODO: ask consent or report error (possible attack)?
+                die("invalid ID Token");
+            }
+        }
+
+        $validatedPostLogoutUri = $this->validatePostLogoutUri($postLogoutUri, $idToken);
+        if ($postLogoutUri) {
+            $postLogoutUri = $this->addStateToUri($postLogoutUri, $request->get('state', null));
+        }
+
+        if ($getConsent) {
+            var_dump("do you wanna leave?");
+            if ($postLogoutUri) {
+                var_dump("and continue to $postLogoutUri afterwards?");
+            }
+        } else {
+            var_dump("logged out!");
+            if ($postLogoutUri && !$validatedPostLogoutUri) {
+                var_dump("Do you want to go to $postLogoutUri ?");
+            }
+        }
+
+        if ($postLogoutUri) {
+            var_dump($postLogoutUri);
+        }
+
+        die();
     }
 
     /**
@@ -118,5 +153,74 @@ class SessionManagementController extends Controller
             : '';
 
         return "$scheme$user$pass$host$port$path$query$fragment";
+    }
+
+    /**
+     * @param mixed $idToken a JWT ID Token as a \JOSE_JWT object or string
+     * @return bool true if $idToken is valid, false otherwise
+     */
+    private function checkIdToken($idToken)
+    {
+        $idToken = $this->getIdToken($idToken);
+
+        /** @var PublicKey $publicKeyStorage */
+        $publicKeyStorage = $this->get('oauth2.storage.public_key');
+        try {
+            @$idToken->verify($publicKeyStorage->getPublicKey($idToken->claims['aud']));
+
+            return true;
+        } catch (\JOSE_Exception_VerificationFailed $e) {
+            // TODO: ask consent or report error (possible attack)?
+            die("invalid ID Token");
+        } catch (\Exception $e) {
+            // TODO: ask consent or report error (possible attack)?
+            die("other error");
+        }
+    }
+
+    /**
+     * Enforces that the ID Token is a \JOSE_JWT object
+     * @param mixed $idToken
+     * @return \JOSE_JWE|\JOSE_JWT
+     */
+    private function getIdToken($idToken)
+    {
+        if (!($idToken instanceof \JOSE_JWT)) {
+            $idToken = \JOSE_JWT::decode($idToken);
+        }
+
+        return $idToken;
+    }
+
+    private function validatePostLogoutUri($postLogoutUri, $idToken)
+    {
+        if ($idToken === null || $postLogoutUri === null) {
+            return false;
+        }
+
+        $idToken = $this->getIdToken($idToken);
+        $client = $this->getClient($idToken->claims['aud']);
+        // TODO: get client's allowed post_logout_uri and check if it contains $postLogoutUri
+        var_dump($client->getName());
+
+        $validatedPostLogoutUri = true;
+    }
+
+    private function addStateToUri($postLogoutUri, $state)
+    {
+        if ($state) {
+            $url = parse_url($postLogoutUri);
+            if (array_key_exists('query', $url)) {
+                parse_str($url['query'], $query);
+            } else {
+                $query = [];
+            }
+            $query['state'] = $state;
+            $url['query'] = http_build_query($query);
+
+            return $this->unparseUrl($url);
+        } else {
+            return $postLogoutUri;
+        }
     }
 }
