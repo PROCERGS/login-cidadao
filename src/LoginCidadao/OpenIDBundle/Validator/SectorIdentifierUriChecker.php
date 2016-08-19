3b7a168f8601a806c2f49063a5eefc438519703b
Guilherme Donato <guilhermednt@users.noreply.github.com>
2016-08-19 10:34:53 -0300

    better handling of sector_identifier_uri outages #456

--- src/LoginCidadao/OpenIDBundle/Validator/SectorIdentifierUriChecker.php
+++ src/LoginCidadao/OpenIDBundle/Validator/SectorIdentifierUriChecker.php
@@ -12,6 +12,7 @@ namespace LoginCidadao\OpenIDBundle\Validator;
 
 use Doctrine\ORM\EntityManager;
 use LoginCidadao\OpenIDBundle\Entity\ClientMetadata;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SectorIdentifierUriChecker
 {
@@ -31,6 +32,7 @@ class SectorIdentifierUriChecker
      * @param ClientMetadata $metadata
      * @param $sectorIdentifierUri
      * @return bool
+     * @throws HttpException
      */
     public function check(ClientMetadata $metadata, $sectorIdentifierUri)
     {
@@ -38,6 +40,11 @@ class SectorIdentifierUriChecker
         curl_setopt($ch, CURLOPT_URL, $sectorIdentifierUri);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $response = curl_exec($ch);
+        $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if ($responseCode !== 200) {
+            throw new HttpException($responseCode);
+        }
 
         $allowedUris = json_decode(trim($response));
 
@@ -58,11 +65,17 @@ class SectorIdentifierUriChecker
     {
         $url = $metadata->getSectorIdentifierUri();
 
-        if ($url !== null && !$this->check($metadata, $url)) {
-            $metadata->setOrganization(null);
-            $metadata->setSectorIdentifierUri(null);
-            $this->em->persist($metadata);
-            $this->em->flush($metadata);
+        try {
+            if ($url !== null && !$this->check($metadata, $url)) {
+                $metadata->setOrganization(null);
+                $metadata->setSectorIdentifierUri(null);
+                $this->em->persist($metadata);
+                $this->em->flush($metadata);
+            }
+        } catch (HttpException $e) {
+            if ($e->getStatusCode() !== 200) {
+                $metadata->setOrganization(null);
+            }
         }
 
         return $metadata;
