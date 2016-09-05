9665fc4e823d824d186f8b841fe833780d4521fd
Guilherme Donato <guilhermednt@users.noreply.github.com>
2016-09-05 15:42:58 -0300

    using workaround until ramsey/uuid is not fixed #485

---
+++ src/LoginCidadao/CoreBundle/Security/Compatibility/RamseyUuidFeatureSet.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the login-cidadao project or it's bundles.
+ *
+ * (c) Guilherme Donato <guilhermednt on github>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LoginCidadao\CoreBundle\Security\Compatibility;
+
+use Ramsey\Uuid\FeatureSet;
+use Ramsey\Uuid\Provider\Node\RandomNodeProvider;
+
+class RamseyUuidFeatureSet extends FeatureSet
+{
+    protected function buildNodeProvider()
+    {
+        return new RandomNodeProvider();
+    }
+}
