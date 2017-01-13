591ac284d14a66361be807543f3c6eebd805907f
Guilherme Donato <guilhermednt@users.noreply.github.com>
2017-01-13 15:14:15 -0200

    using annotation routing #608

--- src/PROCERGS/LoginCidadao/AccountingBundle/Controller/DefaultController.php
+++ src/PROCERGS/LoginCidadao/AccountingBundle/Controller/DefaultController.php
@@ -2,12 +2,24 @@
 
 namespace PROCERGS\LoginCidadao\AccountingBundle\Controller;
 
+use LoginCidadao\OAuthBundle\Entity\AccessTokenRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * @Route("/accounting", name="lc_accounting_data")
+     */
+    public function indexAction()
     {
-        return $this->render('PROCERGSLoginCidadaoAccountingBundle:Default:index.html.twig', array('name' => $name));
+        /** @var AccessTokenRepository $repo */
+        $repo = $this->getDoctrine()->getRepository('LoginCidadaoOAuthBundle:AccessToken');
+
+        $start = new \DateTime('2017-01-01');
+        $end = new \DateTime('2017-02-01');
+        $data = $repo->getAccounting($start, $end);
+        var_dump($data);
+        die();
     }
 }
