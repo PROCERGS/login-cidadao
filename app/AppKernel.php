60580079e1c8abca8c89e8638adf840c4d4c2c43
Guilherme Donato <guilhermednt@users.noreply.github.com>
2017-01-27 17:53:17 -0200

    added password validation rules #585

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -58,6 +58,7 @@ class AppKernel extends Kernel
 
             new Donato\PathWellBundle\DonatoPathWellBundle(),
             new Misd\PhoneNumberBundle\MisdPhoneNumberBundle(),
+            new Rollerworks\Bundle\PasswordStrengthBundle\RollerworksPasswordStrengthBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
