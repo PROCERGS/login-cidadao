66b19eb7832ca896fe3464cc3cc8859d8c62b663
Guilherme Donato <guilhermednt@users.noreply.github.com>
2017-04-04 17:26:51 -0300

    created SmsServiceBundle PROCERGS#657

---
+++ src/PROCERGS/SmsServiceBundle/Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * This file is part of the login-cidadao project or it's bundles.
+ *
+ * (c) Guilherme Donato <guilhermednt on github>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: gdnt
+ * Date: 04/04/17
+ * Time: 11:41
+ */
+
+namespace PROCERGS\SmsServiceBundle\Tests\DependencyInjection;
+
+
+use PROCERGS\SmsServiceBundle\DependencyInjection\Configuration;
+use Symfony\Component\Config\Definition\Processor;
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    public static function getConfig()
+    {
+        return [
+            'uri' => [
+                'send' => 'https://some.address/send',
+                'receive' => 'https://some.address/send',
+                'status' => 'https://some.address/send',
+            ],
+            'system' => [
+                'id' => 'SYSTEM',
+                'key' => 'SECRET_KEY',
+            ],
+            'service_order' => 1234,
+            'from' => 'IdP',
+        ];
+    }
+
+    public function testGetConfigTreeBuilder()
+    {
+        $configuration = new Configuration();
+        $this->assertInstanceOf(
+            'Symfony\Component\Config\Definition\Builder\TreeBuilder',
+            $configuration->getConfigTreeBuilder()
+        );
+    }
+
+    public function testEmptyConfig()
+    {
+        $this->setExpectedException('Symfony\Component\Config\Definition\Exception\InvalidConfigurationException');
+        $processor = new Processor();
+        $processor->processConfiguration(new Configuration(), []);
+    }
+
+    public function testFullConfig()
+    {
+        $processor = new Processor();
+        $expected = $this->getConfig();
+        $config = $processor->processConfiguration(new Configuration(), [$expected]);
+        $this->assertEquals($expected, $config);
+    }
+}
---
+++ src/PROCERGS/SmsServiceBundle/Tests/DependencyInjection/PROCERGSSmsServiceExtensionTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * This file is part of the login-cidadao project or it's bundles.
+ *
+ * (c) Guilherme Donato <guilhermednt on github>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PROCERGS\SmsServiceBundle\Tests\DependencyInjection;
+
+use PROCERGS\SmsServiceBundle\DependencyInjection\PROCERGSSmsServiceExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+
+class PROCERGSSmsServiceExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    private function createContainer()
+    {
+        $container = new ContainerBuilder(
+            new ParameterBag(
+                [
+                    'kernel.cache_dir' => __DIR__,
+                    'kernel.root_dir' => __DIR__.'/Fixtures',
+                    'kernel.charset' => 'UTF-8',
+                    'kernel.debug' => false,
+                ]
+            )
+        );
+
+        return $container;
+    }
+
+    private function compileContainer(ContainerBuilder $container)
+    {
+        $container->getCompilerPassConfig()->setOptimizationPasses([]);
+        $container->getCompilerPassConfig()->setRemovingPasses([]);
+        $container->compile();
+    }
+
+    public function testParametersLoaded()
+    {
+        $config = ConfigurationTest::getConfig();
+
+        $container = $this->createContainer();
+        $container->registerExtension(new PROCERGSSmsServiceExtension());
+        $container->loadFromExtension('procergs_sms_service', $config);
+        $this->compileContainer($container);
+
+        $this->assertEquals($config['uri']['send'], $container->getParameter('procergs.sms.uri.send'));
+        $this->assertEquals($config['uri']['receive'], $container->getParameter('procergs.sms.uri.receive'));
+        $this->assertEquals($config['uri']['status'], $container->getParameter('procergs.sms.uri.status'));
+
+        $this->assertEquals($config['system']['id'], $container->getParameter('procergs.sms.system.id'));
+        $this->assertEquals($config['system']['key'], $container->getParameter('procergs.sms.system.key'));
+
+        $this->assertEquals($config['service_order'], $container->getParameter('procergs.sms.service_order'));
+        $this->assertEquals($config['from'], $container->getParameter('procergs.sms.from.prefix'));
+    }
+}
