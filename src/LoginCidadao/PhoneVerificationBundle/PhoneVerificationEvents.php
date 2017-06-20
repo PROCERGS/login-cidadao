e79f5a85fda5e57373561f0c282c64e153aaafd2
Guilherme Donato <guilhermednt@users.noreply.github.com>
2017-06-20 11:18:13 -0300

    decoupled from PROCERGS's code PROCERGS#702

--- src/LoginCidadao/PhoneVerificationBundle/PhoneVerificationEvents.php
+++ src/LoginCidadao/PhoneVerificationBundle/PhoneVerificationEvents.php
@@ -40,4 +40,9 @@ class PhoneVerificationEvents
      * Ideally this should be run only in batch situations since it's potentially a long-running task!
      */
     const PHONE_VERIFICATION_UPDATE_SENT_VERIFICATIONS = 'lc.phone_verification.update_sent_verifications';
+
+    /**
+     * This event is triggered when there is a need to get the status of sent verifications.
+     */
+    const PHONE_VERIFICATION_GET_SENT_VERIFICATION_STATUS = 'lc.phone_verification.get_sent_verification_status';
 }
---
+++ src/LoginCidadao/PhoneVerificationBundle/Service/SmsStatusUpdater.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * This file is part of the login-cidadao project or it's bundles.
+ *
+ * (c) Guilherme Donato <guilhermednt on github>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LoginCidadao\PhoneVerificationBundle\Service;
+
+use Doctrine\ORM\EntityManager;
+use LoginCidadao\PhoneVerificationBundle\Entity\SentVerificationRepository;
+use LoginCidadao\PhoneVerificationBundle\Event\UpdateStatusEvent;
+use LoginCidadao\PhoneVerificationBundle\Model\DeliveryStatus;
+use LoginCidadao\PhoneVerificationBundle\Model\SentVerificationInterface;
+use LoginCidadao\PhoneVerificationBundle\PhoneVerificationEvents;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class SmsStatusUpdater
+{
+    /** @var EventDispatcherInterface */
+    private $dispatcher;
+
+    /** @var SentVerificationRepository */
+    private $sentVerificationRepo;
+
+    /**
+     * SmsStatusUpdater constructor.
+     * @param EventDispatcherInterface $dispatcher
+     * @param SentVerificationRepository $sentVerificationRepo
+     */
+    public function __construct(EventDispatcherInterface $dispatcher, SentVerificationRepository $sentVerificationRepo)
+    {
+        $this->dispatcher = $dispatcher;
+        $this->sentVerificationRepo = $sentVerificationRepo;
+    }
+
+    public function updateSentVerificationStatus(SymfonyStyle $io, EntityManager $em = null)
+    {
+        $count = $this->sentVerificationRepo->countPendingUpdateSentVerification();
+
+        if ($count === 0) {
+            $io->comment('No messages pending update.');
+
+            return [];
+        }
+
+        $query = $this->sentVerificationRepo->getPendingUpdateSentVerificationQuery();
+        $sentVerifications = $query->iterate();
+
+        $io->progressStart($count);
+        $transactionsUpdated = [];
+        foreach ($sentVerifications as $row) {
+            /** @var SentVerificationInterface $sentVerification */
+            $sentVerification = $row[0];
+            $status = $this->getStatus($sentVerification->getTransactionId());
+
+            if (false === $status->isUpdated()) {
+                $io->progressAdvance(1);
+                continue;
+            }
+
+            $deliveredAt = $status->getDeliveredAt();
+            $sentVerification->setActuallySentAt($status->getSentAt())
+                ->setDeliveredAt($deliveredAt)
+                ->setFinished(
+                    $deliveredAt instanceof \DateTime || DeliveryStatus::isFinal($status->getDeliveryStatus())
+                );
+            $em->flush();
+            $em->clear();
+            $transactionsUpdated[] = $sentVerification->getTransactionId();
+            $io->progressAdvance(1);
+        }
+        $io->progressFinish();
+
+        $countUpdated = count($transactionsUpdated);
+        $io->comment("Updated {$countUpdated} transactions.");
+
+        if ($countUpdated === 0) {
+            $io->comment("It's possible the SMS-sending service you are using doesn't implement status updates.");
+        }
+
+        return $transactionsUpdated;
+    }
+
+    /**
+     * @param $amount
+     * @return float average delivery time in seconds (abs value)
+     */
+    public function getAverageDeliveryTime($amount)
+    {
+        /** @var SentVerificationInterface[] $sentVerifications */
+        $sentVerifications = $this->sentVerificationRepo->getLastDeliveredVerifications($amount);
+
+        if (count($sentVerifications) === 0) {
+            return 0;
+        }
+
+        $times = [];
+        foreach ($sentVerifications as $sentVerification) {
+            $times[] = $sentVerification->getDeliveredAt()->format('U') - $sentVerification->getSentAt()->format('U');
+        }
+        $sum = array_sum($times);
+
+        $avg = $sum / count($times);
+
+        return abs($avg / 60);
+    }
+
+    /**
+     * @param $transactionId
+     * @return UpdateStatusEvent
+     */
+    private function getStatus($transactionId)
+    {
+        $event = new UpdateStatusEvent($transactionId);
+        $this->dispatcher->dispatch(PhoneVerificationEvents::PHONE_VERIFICATION_GET_SENT_VERIFICATION_STATUS, $event);
+
+        return $event;
+    }
+}
