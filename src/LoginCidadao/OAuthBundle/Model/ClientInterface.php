dade00240daa77c5ce45a0d3306276799538fe00
Guilherme Donato <guilhermednt@users.noreply.github.com>
2018-04-23 11:06:41 -0300

    added missing method to fix Scrutinizer issue PROCERGS#768

--- src/LoginCidadao/OAuthBundle/Model/ClientInterface.php
+++ src/LoginCidadao/OAuthBundle/Model/ClientInterface.php
@@ -99,6 +99,12 @@ interface ClientInterface extends BaseInterface
     public function getMetadata();
 
     /**
+     * @param ClientMetadata $metadata
+     * @return ClientInterface
+     */
+    public function setMetadata(ClientMetadata $metadata);
+
+    /**
      * @param string[] $allowedScopes
      * @return ClientInterface
      */
