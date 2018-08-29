5f1f96e72aab6b6eac10cee356b95b030f073939
Guilherme Donato <guilhermednt@users.noreply.github.com>
2018-08-29 15:18:40 -0300

    fixed more forms PROCERGS#808

--- src/LoginCidadao/CoreBundle/Form/Type/TwoFactorAuthenticationBackupCodeGenerationFormType.php
+++ src/LoginCidadao/CoreBundle/Form/Type/TwoFactorAuthenticationBackupCodeGenerationFormType.php
@@ -1,7 +1,18 @@
 <?php
+/**
+ * This file is part of the login-cidadao project or it's bundles.
+ *
+ * (c) Guilherme Donato <guilhermednt on github>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace LoginCidadao\CoreBundle\Form\Type;
 
+use LoginCidadao\CoreBundle\Entity\Person;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,26 +24,24 @@ class TwoFactorAuthenticationBackupCodeGenerationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('current_password', 'password',
-                array(
+            ->add('current_password', PasswordType::class, [
                 'label' => 'To prevent unauthorized Backup Code generation type your password to confirm:',
-                'attr' => array('autocomplete' => 'off'),
+                'attr' => ['autocomplete' => 'off'],
                 'required' => true,
                 'constraints' => new UserPassword(),
-                'mapped' => false
-            ))
-            ->add('generate', 'submit',
-                array(
-                'attr' => array('class' => 'btn btn-success'),
-                'label' => 'Generate new Backup Codes')
-        );
+                'mapped' => false,
+            ])
+            ->add('generate', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-success'],
+                'label' => 'Generate new Backup Codes',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(array(
-            'data_class' => 'LoginCidadao\CoreBundle\Entity\Person'
-        ));
+        $resolver->setDefaults([
+            'data_class' => Person::class,
+        ]);
     }
 
     public function getName()
