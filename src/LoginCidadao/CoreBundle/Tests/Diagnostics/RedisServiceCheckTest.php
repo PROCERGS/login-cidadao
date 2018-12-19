9757392fb353fed639c8c71579638233a8e8f05f
Guilherme Donato <guilhermednt@users.noreply.github.com>
2018-12-19 18:26:07 -0200

    created Predis Redis check PROCERGS#824

---
+++ src/LoginCidadao/CoreBundle/Tests/Diagnostics/RedisServiceCheckTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of the login-cidadao project or it's bundles.
+ *
+ * (c) Guilherme Donato <guilhermednt on github>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LoginCidadao\CoreBundle\Tests\Diagnostics;
+
+use PHPUnit\Framework\TestCase;
+
+class RedisServiceCheckTest extends TestCase
+{
+    public function testRedisCheck()
+    {
+        $this->fail('Not implemented yet');
+    }
+}
