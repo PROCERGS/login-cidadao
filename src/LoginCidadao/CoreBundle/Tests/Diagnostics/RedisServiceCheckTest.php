a8a71408fecb2f563b7bfd11f7fbd83de1dae65f
Guilherme Donato <guilhermednt@users.noreply.github.com>
2018-12-20 14:12:17 -0200

    added tests for RedisServiceCheck PROCERGS#824

--- src/LoginCidadao/CoreBundle/Tests/Diagnostics/RedisServiceCheckTest.php
+++ src/LoginCidadao/CoreBundle/Tests/Diagnostics/RedisServiceCheckTest.php
@@ -10,12 +10,73 @@
 
 namespace LoginCidadao\CoreBundle\Tests\Diagnostics;
 
+use LoginCidadao\CoreBundle\Diagnostics\RedisServiceCheck;
 use PHPUnit\Framework\TestCase;
+use Predis\ClientException;
+use ZendDiagnostics\Result\Failure;
+use ZendDiagnostics\Result\Success;
+use ZendDiagnostics\Result\Warning;
 
+/**
+ * @group time-sensitive
+ */
 class RedisServiceCheckTest extends TestCase
 {
-    public function testRedisCheck()
+    public function testNoService()
     {
-        $this->fail('Not implemented yet');
+        $result = (new RedisServiceCheck(null))->check();
+
+        $this->assertInstanceOf(Warning::class, $result);
+    }
+
+    public function testSuccess()
+    {
+        $key = null;
+        $value = null;
+        $redis = $this->createMock(RedisClientMockInterface::class);
+
+        $redis->expects($this->once())->method('set')
+            ->willReturnCallback(function ($usedKey, $usedValue) use (&$key, &$value) {
+                $key = $usedKey;
+                $value = $usedValue;
+            });
+
+        $redis->expects($this->once())->method('get')
+            ->willReturnCallback(function ($getKey) use (&$key, &$value) {
+                $this->assertSame($key, $getKey);
+
+                return $value;
+            });
+
+        $redis->expects($this->once())->method('del')->with($this->isType('array'))
+            ->willReturnCallback(function ($delKey) use (&$key, &$value) {
+                $this->assertSame($key, $delKey[0]);
+
+                return true;
+            });
+
+        $result = (new RedisServiceCheck($redis))->check();
+        $this->assertInstanceOf(Success::class, $result);
+    }
+
+    public function testException()
+    {
+        $redis = $this->createMock(RedisClientMockInterface::class);
+
+        $redis->expects($this->once())->method('set')->willThrowException(new ClientException());
+        $result = (new RedisServiceCheck($redis))->check();
+
+        $this->assertInstanceOf(Failure::class, $result);
+    }
+
+    public function testUnexpectedResponse()
+    {
+        $redis = $this->createMock(RedisClientMockInterface::class);
+
+        $redis->expects($this->once())->method('set');
+        $redis->expects($this->once())->method('get')->willReturn('surprise!');
+        $result = (new RedisServiceCheck($redis))->check();
+
+        $this->assertInstanceOf(Failure::class, $result);
     }
 }
